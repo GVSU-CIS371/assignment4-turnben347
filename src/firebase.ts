8b00ddfa24a0861ef59a67b486f200455444287b
turnben347 <turneben@mail.gvsu.edu>
2025-11-17 15:42:39 -0500

    Replace Firebase config with real credentials
    Updated Firebase configuration with actual credentials.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,17 @@
-import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  //   // COPY this from your Firebase Console
-  //   apiKey: "your-api-key-goes-here",
-  //   authDomain: "your-project-name-here.firebaseapp.com",
-  //   databaseURL: "https://your-project-name-here.firebaseio.com",
-  //   projectId: "your-project-name-here",
-  //   storageBucket: "your-project-name.appspot.com",
-  //   messagingSenderId: "xxxxxxxx",
+  apiKey: "AIzaSyCrBznc7GwQhXwQpS3qpLk3tCUhIQKnTLg",
+  authDomain: "cis371-a2b0d.firebaseapp.com",
+  projectId: "cis371-a2b0d",
+  storageBucket: "cis371-a2b0d.firebasestorage.app",
+  messagingSenderId: "273703238164",
+  appId: "1:273703238164:web:6a053b1d21b7de5309c501"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export default db;
